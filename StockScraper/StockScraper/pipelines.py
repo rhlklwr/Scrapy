0df30aa340633a7de6ac2f65fff880760b6c02f5
rhlklwr <kalwar.rahul@gmail.com>
2020-04-19 18:50:17 +0530

    Moneycontrol

---
+++ StockScraper/StockScraper/pipelines.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+
+class FireStore(object):
+
+    def open_spider(self, spider):
+        # Authentication with Firebase
+        self.cred = credentials.Certificate("./ServiceAccountKey.json")
+        self.app = firebase_admin.initialize_app(self.cred)
+
+        # Instantiate Firestore class
+        self.db = firestore.client()
+
+        # Instantiate batch class to update data in single batch
+        self.batch = self.db.batch()
+
+    def close_spider(self, spider):
+        self.batch.commit()
+
+    def process_item(self, item, spider):
+        top_news = self.db.collection(u'News').document(u'Moneycontrol')
+        market_action = self.db.collection(u'Market_Index').document(u'Indexes')
+        nse_most_active = self.db.collection(u'nse').document(u'most_active')
+        bse_most_active = self.db.collection(u'bse').document(u'most_active')
+        nse_top_gainers = self.db.collection(u'nse').document(u'top_gainers')
+        bse_top_gainers = self.db.collection(u'bse').document(u'top_gainers')
+        nse_top_losers = self.db.collection(u'nse').document(u'top_losers')
+        bse_top_losers = self.db.collection(u'bse').document(u'top_losers')
+
+        self.batch.update(top_news, item['news']["0"])
+        self.batch.update(market_action, item["market_action_index"])
+        self.batch.update(nse_most_active, item["nse_most_active"])
+        self.batch.update(bse_most_active, item["bse_most_active"])
+        self.batch.update(nse_top_gainers, item["nse_top_gainers"])
+        self.batch.update(bse_top_gainers, item["bse_top_gainers"])
+        self.batch.update(nse_top_losers, item["nse_top_losers"])
+        self.batch.update(bse_top_losers, item["bse_top_losers"])
+
+        return item
---
+++ StockScraper/StockScraper/spiders/moneycontrol.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class MoneycontrolSpider(scrapy.Spider):
+    name = 'moneycontrol'
+    allowed_domains = ['www.moneycontrol.com']
+    start_urls = ['http://www.moneycontrol.com']
+
+    def parse(self, response):
+        top_news = response.xpath("//ul[@class='tabs_nwsconlist']/li/a")[0:5]
+        market_action = response.xpath("//table[@class='rhsglTbl']/tbody")[0].xpath(".//tr")
+        market_action_index = market_action.xpath(".//td//h3/a/@title")
+        nse_most_active = response.xpath("//table[@class='rhsglTbl']/tbody")[2].xpath(".//tr")
+        nse_most_active_companies = nse_most_active.xpath(".//td/a/@title")
+        bse_most_active = response.xpath("//table[@class='rhsglTbl']/tbody")[3].xpath(".//tr")
+        bse_most_active_companies = bse_most_active.xpath(".//td/a/@title")
+        nse_top_gainers = response.xpath("//table[@class='rhsglTbl']/tbody")[4].xpath(".//tr")
+        nse_top_gainers_companies = nse_top_gainers.xpath(".//td/a/@title")
+        bse_top_gainers = response.xpath("//table[@class='rhsglTbl']/tbody")[5].xpath(".//tr")
+        bse_top_gainers_companies = bse_top_gainers.xpath(".//td/a/@title")
+        nse_top_losers = response.xpath("//table[@class='rhsglTbl']/tbody")[6].xpath(".//tr")
+        nse_top_losers_companies = nse_top_losers.xpath(".//td/a/@title")
+        bse_top_losers = response.xpath("//table[@class='rhsglTbl']/tbody")[7].xpath(".//tr")
+        bse_top_losers_companies = bse_top_losers.xpath(".//td/a/@title")
+
+        for idx, news in enumerate(top_news):
+            link = news.xpath(".//@href").get()
+            title = news.xpath(".//@title").get()
+            yield {
+                "news": {
+                    f"{idx + 1}": {
+                        "link": link,
+                        "title": title
+                    }
+                }
+            }
+        for idx, companies, entries in zip(range(1, 6), nse_most_active_companies, nse_most_active):
+            company_name = companies.get()
+            price = entries.xpath(".//td")[1].xpath(".//text()").get()
+            change = entries.xpath(".//td")[2].xpath(".//text()").get()
+            value = entries.xpath(".//td")[3].xpath(".//text()").get()
+            yield {
+                "nse_most_active": {
+                    f"{idx}": {
+                        "Company": company_name,
+                        "Price": price,
+                        "Change": change,
+                        "Value": value
+                    }
+                }
+            }
+
+        for idx, companies, entries in zip(range(1, 6), bse_most_active_companies, bse_most_active):
+            company_name = companies.get()
+            price = entries.xpath(".//td")[1].xpath(".//text()").get()
+            change = entries.xpath(".//td")[2].xpath(".//text()").get()
+            value = entries.xpath(".//td")[3].xpath(".//text()").get()
+            yield {
+                "bse_most_active": {
+                    f"{idx}": {
+                        "Company": company_name,
+                        "Price": price,
+                        "Change": change,
+                        "Value": value
+                    }
+                }
+            }
+        for idx, companies, entries in zip(range(1, 6), nse_top_gainers_companies, nse_top_gainers):
+            company_name = companies.get()
+            price = entries.xpath(".//td")[1].xpath(".//text()").get()
+            change = entries.xpath(".//td")[2].xpath(".//text()").get()
+            gain = entries.xpath(".//td")[3].xpath(".//text()").get()
+            yield {
+                "nse_top_gainers": {
+                    f"{idx}": {
+                        "Company": company_name,
+                        "Price": price,
+                        "Change": change,
+                        "%Gain": gain
+                    }
+                }
+            }
+        for idx, companies, entries in zip(range(1, 6), bse_top_gainers_companies, bse_top_gainers):
+            company_name = companies.get()
+            price = entries.xpath(".//td")[1].xpath(".//text()").get()
+            change = entries.xpath(".//td")[2].xpath(".//text()").get()
+            gain = entries.xpath(".//td")[3].xpath(".//text()").get()
+            yield {
+                "bse_top_gainers": {
+                    f"{idx}": {
+                        "Company": company_name,
+                        "Price": price,
+                        "Change": change,
+                        "%Gain": gain
+                    }
+                }
+            }
+        for idx, companies, entries in zip(range(1, 6), nse_top_losers_companies, nse_top_losers):
+            company_name = companies.get()
+            price = entries.xpath(".//td")[1].xpath(".//text()").get()
+            change = entries.xpath(".//td")[2].xpath(".//text()").get()
+            loss = entries.xpath(".//td")[3].xpath(".//text()").get()
+            yield {
+                "nse_top_losers": {
+                    f"{idx}": {
+                        "Company": company_name,
+                        "Price": price,
+                        "Change": change,
+                        "%Loss": loss
+                    }
+                }
+            }
+        for idx, companies, entries in zip(range(1, 6), bse_top_losers_companies, bse_top_losers):
+            company_name = companies.get()
+            price = entries.xpath(".//td")[1].xpath(".//text()").get()
+            change = entries.xpath(".//td")[2].xpath(".//text()").get()
+            loss = entries.xpath(".//td")[3].xpath(".//text()").get()
+            yield {
+                "bse_top_losers": {
+                    f"{idx}": {
+                        "Company": company_name,
+                        "Price": price,
+                        "Change": change,
+                        "%Loss": loss
+                    }
+                }
+            }
+        for idx, indexes, entries in zip(range(1, 6), market_action_index, market_action):
+            index = indexes.get()
+            price = entries.xpath(".//td")[1].xpath(".//text()").get()
+            change = entries.xpath(".//td")[2].xpath(".//text()").get()
+            percent_change = entries.xpath(".//td")[3].xpath(".//text()").get()
+            yield {
+                "market_action_index": {
+                    f"{idx}": {
+                        "Index": index,
+                        "Price": price,
+                        "Change": change,
+                        "%Change": percent_change
+                    }
+                }
+            }
